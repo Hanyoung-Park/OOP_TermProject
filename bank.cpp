5bb3bdc627b4b9cb04a194efc931d15f44df9fda
amber1350 <97605655+amber1350@users.noreply.github.com>
2022-11-27 03:25:48 +0900

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -1,38 +1,74 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
+
 using namespace std;
 
 
 class ATM;
 class Account;
 
-int Bank::deposit = 0;
+
+
 class Bank {
 protected:
     string bankName;
-    static int deposit;
+    map<string, vector<Account*>> user_info;
+
 public:
     Bank();
     ~Bank();
     int validPassword(Account userAccount, int num);
+    Account* returnAccount(string userName);
     Account* openAccount();
+
 };
+
 
 Bank::Bank() {}
 Bank::~Bank() {}
+Account* Bank::returnAccount(string userName) {
+    int inputpassword;
+    cout << "Password?" << endl;
+    cin >> inputpassword;
+    for (vector<Account*>::iterator itr = user_info[userName].begin(); itr != user_info[userName].end; ++itr) {
+        if(itr->password == inputpassword) {
+        return itr;
+        }
+    }
+    return nullptr;
+    
+}
+
 Account* Bank::openAccount() {
     string bankName;
     string userName;
-    int Accountnum;
+    int accountNum;
+    int password;
+    int fund;
     cout << "input Bank Name: " << endl;
     cin >> bankName;
     cout << "input User Name: " << endl;
     cin >> userName;
     cout << "input Account Number(12-digit): " << endl;
-    cin >> Accountnum;
+    cin >> accountNum;
+    cout << "input Password: " << endl;
+    cin >> password;
+    cout << "input available fund: " << endl;
+    cin >> fund;
     Account* newAccount;
+    newAccount = new Account(bankName, userName, accountNum, password, fund); //Account class에 password 추가
+    if (user_info.count(userName)) {
+        user_info[userName].push_back(newAccount);
+        }
+    else {
+        vector<Account*> v_account = {newAccount};
+        user_info.insert(pair<string, vector<Account*>(userName, v_account));
+        }
     return newAccount; 
 }
+
 
 int Bank::validPassword(Account userAccount, int num) {
     if(num == userAccount.getPassword) {
