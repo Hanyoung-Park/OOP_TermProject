663e3327c0ac9f6f2ad924029629b05b5c4bbe2c
amber1350 <97605655+amber1350@users.noreply.github.com>
2022-11-29 14:20:26 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -183,42 +183,73 @@ void ATM::endSession() {
     usingAccount = NULL;
 }
 
+using namespace std;
 
-int Bank::deposit = 0;
+class ATM;
+class Account;
+
+
+
+
 class Bank {
 protected:
     string bankName;
-    static int deposit;
+    map<string, Account*> account_info;
+
 public:
     Bank();
     ~Bank();
     int validPassword(Account userAccount, int num);
+    Account* returnAccount(string accountNumber);
     Account* openAccount();
+    string getBankName();
+    
+
 };
 
-Bank::Bank() {}
+Bank::Bank(string bankName) {
+    this->bankName = bankName;
+    account_info = map<string Account*>();
+}
+
 Bank::~Bank() {}
+
+string Bank::getBankName() {
+    return bankName;   
+}
+
+Account* Bank::returnAccount(string accountNumber) {
+    int inputpassword;
+    cout << "Password?" << endl;
+    cin >> inputpassword;
+    if (account_info[accountNumber]->getPassword == inputpassword) {
+        return account_info[accountNumber];
+    }
+    return nullptr;
+}
+
 Account* Bank::openAccount() {
     string bankName;
     string userName;
-    int Accountnum;
+    int accountNum;
+    string password;
+    int fund;
     cout << "input Bank Name: " << endl;
     cin >> bankName;
     cout << "input User Name: " << endl;
     cin >> userName;
     cout << "input Account Number(12-digit): " << endl;
-    cin >> Accountnum;
-    Account* newAccount;
-    return newAccount; 
-}
+    cin >> accountNum;
+    cout << "input Password: " << endl;
+    cin >> password;
+    cout << "input available fund: " << endl;
+    cin >> fund;
 
-int Bank::validPassword(Account userAccount, int num) {
-    if(num == userAccount.getPassword) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+    Account* newAccount;
+    newAccount = new Account(bankName, userName, accountNum, fund, password); //Account class에 password 추가
+    account_info.insert(pair<string, Account*>(accountNum, newAccount));
+    return newAccount;
+
 }
 
 class Account
